642f76de31a7c54b132412162601cf1be6cbbc7b
CodingToastBread <mickeymouset@naver.com>
2023-05-07 19:50:29 +0900

    first commit

---
+++ src/main/java/toast/bread/springkafka/config/KafkaConfigProps.java
@@ -0,0 +1,20 @@
+package toast.bread.springkafka.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "devtiro.kafak")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class KafkaConfigProps {
+
+	private String topic;
+	
+}
